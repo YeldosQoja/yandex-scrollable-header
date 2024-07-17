30d34f1e93ca6b70ab1b3e95af688c4e1e79b2a7
YeldosQoja <yeldos.kozha@gmail.com>
2024-07-17 15:04:18 +0500

    Made tabs stick to top.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Animated, {
   Extrapolation,
   interpolate,
@@ -11,7 +11,11 @@ import { Food } from "./Food";
 import { Detail } from "./Detail";
 import { Colors } from "@/Colors";
 import { foods } from "@/foods";
-import { HEADER_DELTA, HEADER_HEIGHT } from "../constants";
+import {
+  HEADER_DELTA,
+  HEADER_HEIGHT,
+  SEGMENTED_CONTROL_HEIGHT,
+} from "../constants";
 
 type MenuProps = {
   y: SharedValue<number>;
@@ -21,13 +25,13 @@ export const Menu = ({ y }: MenuProps) => {
   const headerStyle = useAnimatedStyle(() => ({
     opacity: interpolate(
       y.value,
-      [HEADER_DELTA / 2, HEADER_DELTA],
+      [HEADER_DELTA / 3, HEADER_DELTA - 24],
       [1, 0],
       Extrapolation.CLAMP
     ),
   }));
 
-  const menuStyle = useAnimatedStyle(() => {
+  const segmentedControlStyle = useAnimatedStyle(() => {
     const borderRadius = interpolate(
       y.value,
       [HEADER_DELTA - 24, HEADER_DELTA],
@@ -39,100 +43,116 @@ export const Menu = ({ y }: MenuProps) => {
       borderTopEndRadius: borderRadius,
       borderTopLeftRadius: borderRadius,
       borderTopRightRadius: borderRadius,
+      transform: [
+        {
+          translateY: interpolate(
+            y.value,
+            [0, HEADER_DELTA],
+            [0, -HEADER_DELTA],
+            {
+              extrapolateRight: Extrapolation.CLAMP,
+            }
+          ),
+        },
+      ],
     };
   });
 
   return (
-    <Animated.ScrollView
-      onScroll={({
-        nativeEvent: {
-          contentOffset: { y: value },
-        },
-      }) => {
-        y.value = value;
-      }}
-      style={StyleSheet.absoluteFill}>
-      <Animated.View style={[styles.header, headerStyle]}>
-        <Text style={styles.headerTitle}>Burger King</Text>
-        <View style={styles.details}>
-          <Detail
-            {...{
-              icon: (
-                <MaterialIcons
-                  name="delivery-dining"
-                  size={24}
-                  color={Colors.white}
-                  style={{ marginRight: 6 }}
-                />
-              ),
-              title: "30-40",
-              extraText: "min",
-            }}
-          />
-          <Detail
-            {...{
-              icon: (
-                <Iconicons
-                  name="star"
-                  size={18}
-                  color={Colors.white}
-                  style={{ marginRight: 6 }}
-                />
-              ),
-              title: "4.9",
-              extraText: "200+",
-            }}
-          />
-          <Detail
-            icon={
-              <Iconicons
-                name="ellipsis-horizontal"
-                color={Colors.white}
-                size={24}
-              />
-            }
-          />
-        </View>
-      </Animated.View>
-      <Animated.View style={[styles.menu, menuStyle]}>
-        <ScrollView
-          horizontal
-          contentContainerStyle={{ padding: 16 }}
-          showsHorizontalScrollIndicator={false}>
-          {foods.map(({ title }, index) => (
-            <Pressable
-              style={{
-                ...styles.button,
-                backgroundColor: index === 0 ? "#42413E" : Colors.black,
+    <>
+      <Animated.ScrollView
+        onScroll={({
+          nativeEvent: {
+            contentOffset: { y: value },
+          },
+        }) => {
+          y.value = value;
+        }}
+        showsVerticalScrollIndicator={false}
+        style={StyleSheet.absoluteFill}>
+        <Animated.View style={[styles.header, headerStyle]}>
+          <Text style={styles.headerTitle}>Burger King</Text>
+          <View style={styles.details}>
+            <Detail
+              {...{
+                icon: (
+                  <MaterialIcons
+                    name="delivery-dining"
+                    size={24}
+                    color={Colors.white}
+                    style={{ marginRight: 6 }}
+                  />
+                ),
+                title: "30-40",
+                extraText: "min",
               }}
-              key={title}>
-              <Text
-                style={{
-                  fontSize: 17,
-                  color: index === 0 ? Colors.white : "#42413E",
-                }}>
-                {title}
-              </Text>
-            </Pressable>
-          ))}
-        </ScrollView>
-        {foods.map((food) => (
-          <View
-            key={food.title}
-            style={styles.section}>
-            <Text style={styles.sectionTitle}>{food.title}</Text>
-            <View style={styles.foods}>
-              {food.items.map((food) => (
-                <Food
-                  key={food.name}
-                  food={food}
+            />
+            <Detail
+              {...{
+                icon: (
+                  <Iconicons
+                    name="star"
+                    size={18}
+                    color={Colors.white}
+                    style={{ marginRight: 6 }}
+                  />
+                ),
+                title: "4.9",
+                extraText: "200+",
+              }}
+            />
+            <Detail
+              icon={
+                <Iconicons
+                  name="ellipsis-horizontal"
+                  color={Colors.white}
+                  size={24}
                 />
-              ))}
-            </View>
+              }
+            />
           </View>
+        </Animated.View>
+        <Animated.View style={styles.menu}>
+          {foods.map((food) => (
+            <View
+              key={food.title}
+              style={styles.section}>
+              <Text style={styles.sectionTitle}>{food.title}</Text>
+              <View style={styles.foods}>
+                {food.items.map((food) => (
+                  <Food
+                    key={food.name}
+                    food={food}
+                  />
+                ))}
+              </View>
+            </View>
+          ))}
+        </Animated.View>
+      </Animated.ScrollView>
+      <Animated.ScrollView
+        horizontal
+        style={[styles.segmentedControl, segmentedControlStyle]}
+        contentContainerStyle={{ alignItems: "center" }}
+        showsHorizontalScrollIndicator={false}>
+        {foods.map(({ title }, index) => (
+          <Pressable
+            style={{
+              ...styles.button,
+              backgroundColor: index === 0 ? "#42413E" : Colors.black,
+            }}
+            key={title}>
+            <Text
+              style={{
+                fontSize: 17,
+                color: index === 0 ? Colors.white : "#42413E",
+              }}>
+              {title}
+            </Text>
+          </Pressable>
         ))}
-      </Animated.View>
-    </Animated.ScrollView>
+      </Animated.ScrollView>
+    </>
   );
 };
 
@@ -144,6 +164,7 @@ const styles = StyleSheet.create({
   header: {
     height: HEADER_HEIGHT,
     padding: 12,
+    paddingBottom: SEGMENTED_CONTROL_HEIGHT + 12,
     justifyContent: "flex-end",
   },
   headerTitle: {
@@ -154,6 +175,15 @@ const styles = StyleSheet.create({
   },
   details: {
     flexDirection: "row",
+  },
+  segmentedControl: {
+    ...StyleSheet.absoluteFillObject,
+    top: HEADER_HEIGHT - SEGMENTED_CONTROL_HEIGHT,
+    left: 0,
+    right: 0,
+    paddingHorizontal: 12,
+    height: SEGMENTED_CONTROL_HEIGHT + 12,
+    backgroundColor: Colors.black,
   },
   menu: {
     backgroundColor: Colors.black,
--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,8 @@ import { Dimensions } from "react-native";
 import Constants from "expo-constants";
 
 const { width, height } = Dimensions.get("window");
-export const HEADER_HEIGHT = height * 0.35;
+export const HEADER_HEIGHT = height * 0.4;
 export const TOPBAR_HEIGHT = 40;
-export const HEADER_DELTA = HEADER_HEIGHT - (TOPBAR_HEIGHT + Constants.statusBarHeight);
+export const SEGMENTED_CONTROL_HEIGHT = 64;
+export const HEADER_DELTA = HEADER_HEIGHT - (TOPBAR_HEIGHT + Constants.statusBarHeight) - SEGMENTED_CONTROL_HEIGHT;
 export const CARD_WIDTH = (width - 12 * 2 - 8) / 2;
